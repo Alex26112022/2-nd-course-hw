79cd30e74577d48f2fde46665a519f8407000cf9
Alexey Denisenko <AlexeyDenisenko2703@yandex.ru>
2026-06-30 22:21:03 +0300

    добавил игру викторина

--- games.js
+++ games.js
@@ -3,5 +3,39 @@ const playGame = {
 };
 
 function simpleQuiz() {
-  alert('hello');
+  const quiz = [
+    {
+      question: 'Какой цвет неба?',
+      options: ['1. Красный', '2. Синий', '3. Зеленый'],
+      correctAnswer: 2,
+    },
+    {
+      question: 'Сколько дней в неделе?',
+      options: ['1. Шесть', '2. Семь', '3. Восемь'],
+      correctAnswer: 2,
+    },
+    {
+      question: 'Сколько у человека пальцев на одной руке?',
+      options: ['1. Четыре', '2. Пять', '3. Шесть'],
+      correctAnswer: 2,
+    },
+  ];
+
+  let countTrue = 0;
+  let resultMessage;
+
+  for (let item of quiz) {
+    let userAnswer = prompt(
+      `${item['question']}\n${item['options'].join('\n')}`
+    );
+    if (Number(userAnswer) === item['correctAnswer']) countTrue++;
+  }
+
+  const word = countTrue === 1 ? 'вопрос' : 'вопроса';
+
+  if (countTrue === 0) {
+    resultMessage = `Вы не ответили правильно ни на один вопрос!`;
+  } else resultMessage = `Вы ответили правильно на ${countTrue} ${word}!`;
+
+  alert(resultMessage);
 }
